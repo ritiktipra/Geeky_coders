e6978d01e1cd02a538c7ac968d92e1f51800bac4
rickypanu <rickypanu2005@gmail.com>
2025-06-30 22:21:00 +0530

    pydantic

--- uietbackend/app/schemas/student.py
+++ uietbackend/app/schemas/student.py
@@ -4,11 +4,11 @@ from datetime import date
 class StudentRegister(BaseModel):
     full_name: str
     email: EmailStr
-    phone: constr(regex=r'^\d{10}$')
+    phone: constr(pattern=r'^\d{10}$')
     dob: date
     gender: str
     address: str
-    roll_no: constr(regex=r'^\d+$')
+    roll_no: constr(pattern=r'^\d+$')
     department: str
     course: str
     semester: int

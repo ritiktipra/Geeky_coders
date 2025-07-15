b6fa7bd3f9cba2b03c512104c599d6ad5fcf7625
rickypanu <rickypanu2005@gmail.com>
2025-07-15 07:50:28 +0530

    login

--- uietbackend/app/api/auth.py
+++ uietbackend/app/api/auth.py
@@ -18,11 +18,11 @@ def login_student(data: StudentLoginRequest):
     student = approved_students.find_one({"roll_no": data.roll_no})
     if not student or str(student["dob"]) != str(data.dob):
         raise HTTPException(status_code=401, detail="Invalid credentials")
-    return {"message": "Login successful"}
+    return {"message": "Login successful", "roll_no": data.roll_no}
 
 @router.post("/login/teacher")
 def login_teacher(data: TeacherLoginRequest):
     teacher = approved_teachers.find_one({"employee_id": data.employee_id.upper()})
     if not teacher or str(teacher["dob"]) != str(data.dob):
         raise HTTPException(status_code=401, detail="Invalid credentials")
-    return {"message": "Login successful"}
+    return {"message": "Login successful","employee_id": data.employee_id.upper()}
